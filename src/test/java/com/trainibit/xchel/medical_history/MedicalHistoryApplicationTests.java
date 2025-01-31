df3ac8ebcb3423d8e93da96386a4aad57dd92aeb
Antonio Zarate Rosas <azr230502@gmail.com>
2025-01-31 12:12:16 -0600

    Esqueleto del proyecto con Spring Initializer

---
+++ src/test/java/com/trainibit/xchel/medical_history/MedicalHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainibit.xchel.medical_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
