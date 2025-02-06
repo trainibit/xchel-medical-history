915878bc0c1283787ba08d3f709b5bbce436af99
Antonio Zarate Rosas <azr230502@gmail.com>
2025-02-05 18:54:18 -0600

    Creating repositories

---
+++ src/main/java/com/trainibit/xchel/medical_history/repository/ChronicDiseaseRepository.java
@@ -0,0 +1,17 @@
+package com.trainibit.xchel.medical_history.repository;
+
+import com.trainibit.xchel.medical_history.entity.ChronicDisease;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ChronicDiseaseRepository extends JpaRepository<ChronicDisease, Long> {
+    @NonNull
+    List<ChronicDisease> findAll();
+
+    ChronicDisease findByUuid(UUID uuid);
+}
