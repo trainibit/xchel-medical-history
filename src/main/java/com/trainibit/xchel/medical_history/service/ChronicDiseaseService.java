da7fd370638085a7ad2c0d511d175b3b149d5bfb
Antonio Zarate Rosas <azr230502@gmail.com>
2025-03-02 20:28:19 -0600

    Dockerizing the application with dockerfile, docker-compose and environment variables

--- src/main/java/com/trainibit/xchel/medical_history/service/ChronicDiseaseService.java
+++ src/main/java/com/trainibit/xchel/medical_history/service/ChronicDiseaseService.java
@@ -1,6 +1,5 @@
 package com.trainibit.xchel.medical_history.service;
 
-import com.trainibit.xchel.medical_history.entity.ChronicDisease;
 import com.trainibit.xchel.medical_history.request.ChronicDiseaseRequest;
 import com.trainibit.xchel.medical_history.response.ChronicDiseaseResponse;
 
